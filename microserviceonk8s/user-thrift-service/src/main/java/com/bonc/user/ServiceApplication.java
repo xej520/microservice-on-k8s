5512f52fca9580658ab170ad28044a363168dce4
邢尔军 <xingej@163.com>
2018-08-27 14:24:37 +0800

    创建spring-boot的启动项

---
+++ microserviceonk8s/user-thrift-service/src/main/java/com/bonc/user/ServiceApplication.java
@@ -0,0 +1,11 @@
+package com.bonc.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApplication.class, args);
+    }
+}
