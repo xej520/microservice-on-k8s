4a3d13c96b2d3bedfd8d9aeb7dc23767b839b9d7
邢尔军 <xingej@163.com>
2018-08-27 14:14:18 +0800

    编写mapper接口，

---
+++ microserviceonk8s/user-thrift-service/src/main/java/com/bonc/user/mapper/UserMapper.java
@@ -0,0 +1,25 @@
+package com.bonc.user.mapper;
+
+import com.bonc.thrift.user.UserInfo;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+// mybatis
+@Mapper
+public interface UserMapper {
+    @Select("select id, username, password, real_name as realName, " +
+            "mobile, email from pe_user where id=#{id}")
+    UserInfo getUserById(@Param("id") int id);
+
+    @Select("select id, username, password, real_name as realName, " +
+            "mobile, email from pe_user where username=#{username}")
+    UserInfo getUserByName(@Param("username") String username);
+
+    //    注册接口
+    @Insert("insert into pe_user (username, password, real_name, mobile, email)" +
+            " values (#{u.username}, #{u.password}, #{u.realName},#{u.mobile},#{u.email})")
+    void registerUser(@Param("u") UserInfo userInfo);
+
+}
