c8cbf62d1485492c19fcdda71a87fa67514d97ed
邢尔军 <xingej@163.com>
2018-08-31 19:35:43 +0800

    删除没有的模块

--- message-thrift-python-service/thrift/gen-code.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh bash
-thrift --gen py -out ../  message.thrift
-
-#生成java api
-thrift --gen java -out ../../message-thrift-service-api/src/main/java message.thrift
-
-
-#生成go  API
-thrift --gen go  -out ../../message-thrift-go-service/pkg  message.thrift
