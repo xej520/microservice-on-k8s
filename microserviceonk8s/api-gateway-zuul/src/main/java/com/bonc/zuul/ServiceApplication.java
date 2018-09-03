a52d15b3eaf803eb75b6190ad283fbb3a352d499
邢尔军 <xingej@163.com>
2018-09-03 19:06:46 +0800

    完成api-gateway-zuul的开发，使用了zuul作为路由的功能

---
+++ microserviceonk8s/api-gateway-zuul/src/main/java/com/bonc/zuul/ServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bonc.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceApplication.class, args);
+    }
+}
